bcd9a3cfa02e5f8e4ee25b86ee42033866703398
sebma369 <1594565975@qq.com>
2023-09-04 19:58:44 +0800

    change font

--- src/main/java/com/example/jokerpoker/HelloController.java
+++ src/main/java/com/example/jokerpoker/HelloController.java
@@ -1,14 +1,29 @@
 package com.example.jokerpoker;
 
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.scene.text.Font;
 
-public class HelloController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class HelloController implements Initializable {
     @FXML
     private Label welcomeText;
 
     @FXML
+    private Label labelTitle;
+
+    @FXML
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        String fontPath=HelloController.class.getResource("fonts/spider.ttf").toExternalForm();
+        Font spiderFont = Font.loadFont(fontPath,30);;
+        this.labelTitle.setFont(spiderFont);
     }
 }
