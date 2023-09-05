f7d0107e05e9c611a04472b9d9190a8d477c94de
ReDawn <3330116629@qq.com>
2023-09-05 20:04:44 +0800

    增加了AccountDAO中的数据库检索

---
+++ src/main/java/com/example/jokerpoker/dao/User.java
@@ -0,0 +1,20 @@
+package com.example.jokerpoker.dao;
+
+public class User {
+    private int id;
+    private String username;
+    private String password;
+
+    private int getId() {return id;}
+
+    private void setId() {this.id = id;}
+
+    private String getUsername() {return username;}
+
+    private void setUsername() {this.username = username;}
+
+    private String getPassword() {return password;}
+
+    private void setPassword() {this.password = password;}
+
+}
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,7 +2,9 @@ module com.example.jokerpoker {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.sql;
+    requires commons.dbutils;
 
     opens com.example.jokerpoker to javafx.fxml;
     exports com.example.jokerpoker;
+    exports com.example.jokerpoker.dao;
 }
