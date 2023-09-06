a56a52343cb49e2f3ff1bee60f1e87d481ac828c
ReDawn <3330116629@qq.com>
2023-09-06 11:00:09 +0800

    修改AccountDAO

--- src/main/java/com/example/jokerpoker/dao/AccountDAO.java
+++ src/main/java/com/example/jokerpoker/dao/AccountDAO.java
@@ -9,26 +9,28 @@ import java.sql.Connection;
 import java.sql.SQLException;
 
 public class AccountDAO {
-    public int checkUser(int id,String username,String password) {
+    public int checkUser(String username,String password) {
         Connection cn = DBConnection.getConnect();
         QueryRunner queryRunner = new QueryRunner();
-        String sql = "select * from user where id=? and username=? and password=?";
+        String sql = "select * from user where username=? and password=?";
         ResultSetHandler<User> h = new BeanHandler<User>(User.class);
         int n = -1;
         try {
-            User p = queryRunner.query(cn,sql, h, id, username, password);
+            User p = queryRunner.query(cn,sql, h, username, password);
             if(p!=null) {
                 n = 1;
+            } else {
+                n = -1;
             }
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
-        return 1;
+        return n;
     }
 
     public static void main(String[] args) {
         AccountDAO accountDAO = new AccountDAO();
-        int n = accountDAO.checkUser(1000,"player1","123456");
+        int n = accountDAO.checkUser("player1","123456");
         System.out.println(n);
     }
 }
